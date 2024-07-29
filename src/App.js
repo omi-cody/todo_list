946acd25313a7fb914c094728c9be72f17caa109
AbhigyaShrestha2060 <abhigyashrestha730@gmail.com>
2024-07-29 11:52:31 +0545

    Chaned in app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Sidebar from './components/sidebar/Sidebar';
+import { BrowserRouter as Router, Routes } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <Router>
-      {/* <Sidebar /> */}
-<Navbar></Navbar>      <Routes>
-        {/* Define your routes here */}
-      </Routes>
+      <Navbar></Navbar>
+      <Routes>{/* Define your routes here */}</Routes>
     </Router>
   );
 }
